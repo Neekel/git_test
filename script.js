45d682a44fa80b5f8a4999a79718fc07d9355867
Neekel <spanky3770@gmail.com>
2021-07-03 00:36:03 +0300

    add script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt('How many films?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+const a = prompt('Last film', ''),
+      b = prompt('Your choice', ''),
+      c = prompt('Last film', ''),
+      d = prompt('Your choice', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
