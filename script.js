18c64fe41e8f9e292cd33e82715263f52622717d
Neekel <spanky3770@gmail.com>
2021-07-03 21:28:39 +0300

    if

--- script.js
+++ script.js
@@ -8,11 +8,27 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 2; i++) {
     const a = prompt('Last film', ''),
       b = prompt('Your choice', '');
-    personalMovieDB.movies[a] = b;
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 ){
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+    }
+    else {
+        console.log("error");
+        i--;
+    }    
 }
 
+if (personalMovieDB.count < 10 ) {
+    console.log("1");
+}
+else if (10 < personalMovieDB.count < 30) {
+    console.log("2");
+}
+else 
+    console.log("3");
 
 console.log(personalMovieDB);
